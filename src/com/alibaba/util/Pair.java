6d1e81215e62f7f54b73c29335e34619cf194825
puchunwei <puchunwei@weidian.com>
2015-09-26 13:50:33 +0800

    update pair TwoTuple

---
+++ src/com/alibaba/util/Pair.java
@@ -0,0 +1,53 @@
+package com.alibaba.util;
+
+/**
+ * Pair.java
+ * @author puchunwei
+ *
+ * 描述：Key-Value Pair对，也可以当做二元组使用。
+ */
+public class Pair<K, V> {
+
+    public final K key;
+    public final V value;
+
+    public Pair(K var1, V var2) {
+        this.key = var1;
+        this.value = var2;
+    }
+
+    public String toString() {
+        return this.key + "=" + this.value;
+    }
+
+    public int hashCode() {
+        return this.key.hashCode() * 13 + (this.value == null?0:this.value.hashCode());
+    }
+
+    public boolean equals(Object var1) {
+        if(this == var1) {
+            return true;
+        } else if(!(var1 instanceof Pair)) {
+            return false;
+        } else {
+            Pair var2 = (Pair)var1;
+            if(this.key != null) {
+                if(!this.key.equals(var2.key)) {
+                    return false;
+                }
+            } else if(var2.key != null) {
+                return false;
+            }
+
+            if(this.value != null) {
+                if(!this.value.equals(var2.value)) {
+                    return false;
+                }
+            } else if(var2.value != null) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/com/alibaba/util/TwoTuple.java
@@ -0,0 +1,39 @@
+package com.alibaba.util;
+
+/**
+ * TwoTuple.java
+ *
+ * @version 1.0
+ * @author puchunwei
+ *
+ * 描述：二元组，用来封装结构，当返回会值需要两个对象时使用。
+ */
+public class TwoTuple<A, B> {
+
+    public final A first;
+
+    public final B second;
+
+    public  TwoTuple(A first, B second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{first:");
+        if(first == null)
+            sb.append("null");
+        else
+            sb.append(first.toString());
+        sb.append(" , ");
+        if(second == null)
+            sb.append("null");
+        else
+            sb.append(second.toString());
+        sb.append("}");
+        return sb.toString();
+    }
+}
