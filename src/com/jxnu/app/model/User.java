a68355eea01dc20d0a32cfb564b440723af0f107
ahpuchunwei <ahpuchunwei@163.com>
2016-01-16 11:21:24 +0800

    update

---
+++ src/com/jxnu/app/model/User.java
@@ -0,0 +1,25 @@
+package com.jxnu.app.model;
+
+/**
+ * Created by puchunwei on 16/1/15.
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/com/jxnu/app/util/ClassUtil.java
@@ -0,0 +1,11 @@
+package com.jxnu.app.util;
+
+/**
+ * Created by puchunwei on 16/1/15.
+ */
+public class ClassUtil {
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        System.out.println(User.class.getSimpleName());
+    }
+}
