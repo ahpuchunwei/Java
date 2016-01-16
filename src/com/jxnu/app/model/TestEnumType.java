03a8d55cf35ee2c752479d9613ede9948896e72e
ahpuchunwei <ahpuchunwei@163.com>
2016-01-16 14:04:25 +0800

    update by source tree

---
+++ src/com/jxnu/app/model/TestEnumType.java
@@ -0,0 +1,16 @@
+package com.jxnu.app.model;
+
+/**
+ * Created by puchunwei on 16/1/16.
+ */
+public enum  TestEnumType {
+
+    /**
+     * test1
+     */
+    test1,
+    /**
+     * test2
+     */
+    test2;
+}
---
+++ src/com/jxnu/app/util/EnumClassTest.java
@@ -0,0 +1,12 @@
+package com.jxnu.app.util;
+
+import com.jxnu.app.model.TestEnumType;
+
+/**
+ * Created by puchunwei on 16/1/16.
+ */
+public class EnumClassTest {
+    public static void main(String[] args) throws Exception{
+        System.out.println(TestEnumType.test1);
+    }
+}
