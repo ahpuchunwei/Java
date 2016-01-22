1438cf3db5ed76e96bdaf6ed5df60e95161985ec
ahpuchunwei <ahpuchunwei@163.com>
2016-01-22 21:01:25 +0800

    update httpclient

---
+++ src/com/jxnu/app/util/HttpClientTest.java
@@ -0,0 +1,11 @@
+package com.jxnu.app.util;
+
+
+/**
+ * Created by puchunwei on 16/1/22.
+ */
+public class HttpClientTest {
+    public static void main(String[] args) throws Exception {
+        HttpClient httpClient = new HttpClient();
+    }
+}
