60bb0b5c7afdfb0b97d57a7f8ad6c094196ff062
ahpuchunwei <ahpuchunwei@163.com>
2016-02-06 10:31:05 +0800

    add Lowcase

---
+++ src/com/jxnu/app/util/StringCaseTest.java
@@ -0,0 +1,13 @@
+package com.jxnu.app.util;
+
+/**
+ * Created by puchunwei on 16/2/5.
+ */
+public class StringCaseTest {
+
+    public static void main(String[] args) {
+        String str = "12345A";
+        String s = str.toLowerCase();
+        System.out.println(s);
+    }
+}
